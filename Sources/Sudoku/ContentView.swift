765933967faa5f024d5f37f1ffb7e037817c48e9
Usman Nazir <usman.nazir@guardian.co.uk>
2024-07-27 15:10:18 +0100

    Adds basic grid data

--- Sources/Sudoku/ContentView.swift
+++ Sources/Sudoku/ContentView.swift
@@ -1,12 +1,37 @@
 import SwiftUI
 
+//@Observable
+//struct GridData {
+//    
+//}
+
 public struct ContentView: View {
-    @AppStorage("tab") var tab = Tab.welcome
-    @AppStorage("name") var name = "Skipper"
     @State var appearance = ""
-    @State var isBeating = false
+    
+    @State var row1: [Int] = Array(repeating: 1, count: 9)
+    @State var row2: [Int] = Array(repeating: 2, count: 9)
+    @State var row3: [Int] = Array(repeating: 3, count: 9)
+    
+    @State var row4: [Int] = Array(repeating: 4, count: 9)
+    @State var row5: [Int] = Array(repeating: 5, count: 9)
+    @State var row6: [Int] = Array(repeating: 6, count: 9)
+    
+    @State var row7: [Int] = Array(repeating: 7, count: 9)
+    @State var row8: [Int] = Array(repeating: 8, count: 9)
+    @State var row9: [Int] = Array(repeating: 9, count: 9)
+    
+    @State var gridData: [Binding<[Int]>] = .init()// = [$row1, $row2, $row3]
 
     public init() {
+        self._gridData = State(initialValue: [Binding(projectedValue: $row1),
+                                              Binding(projectedValue: $row2),
+                                              Binding(projectedValue: $row3),
+                                              Binding(projectedValue: $row4),
+                                              Binding(projectedValue: $row5),
+                                              Binding(projectedValue: $row6),
+                                              Binding(projectedValue: $row7),
+                                              Binding(projectedValue: $row8),
+                                              Binding(projectedValue: $row9)])
     }
 
     public var body: some View {
@@ -16,7 +41,7 @@ public struct ContentView: View {
                     .ignoresSafeArea()
                 VStack(alignment: .center) {
                     HeaderView()
-                    GridView()
+                    GridView(data: $gridData)
                         .frame(width: geometry.size.width, height: geometry.size.width)
                     InputView()
                         .frame(height: geometry.size.height*0.35)
--- Sources/Sudoku/Views/Grid.swift
+++ Sources/Sudoku/Views/Grid.swift
@@ -9,152 +9,107 @@ import SwiftUI
 
 struct GridView: View {
     
+    @Binding var data: [Binding<[Int]>]
+    
     var body: some View {
         VStack(spacing: 0) {
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
-            GridRow()
+            GridRow(data: data[0])
+            GridRow(data: data[1])
+            GridRow(data: data[2])
+            GridRow(data: data[3])
+            GridRow(data: data[4])
+            GridRow(data: data[5])
+            GridRow(data: data[6])
+            GridRow(data: data[7])
+            GridRow(data: data[8])
+        }
+        .background {
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Color.blue.opacity(0.05)
+                    Color.blue.opacity(0.15)
+                    Color.blue.opacity(0.05)
+                }
+                HStack(spacing: 0) {
+                    Color.blue.opacity(0.15)
+                    Color.blue.opacity(0.05)
+                    Color.blue.opacity(0.15)
+                }
+                HStack(spacing: 0) {
+                    Color.blue.opacity(0.05)
+                    Color.blue.opacity(0.15)
+                    Color.blue.opacity(0.05)
+                }
+            }
         }
     }
-    
-//    var body: some View {
-//        ZStack {
-//            Color.green
-//            Grid(horizontalSpacing: 0, verticalSpacing: 0) {
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//                GridRow {
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                    GridBox()
-//                }
-//            }
-//        }
-//    }
 }
 
 struct GridBox: View {
+    
+    var value: Int
+    
     var body: some View {
-        Text("  1  ")
-            .frame(width: 40, height: 40)
-            .border(Color.black)
+        Button(action: {
+            print("select")
+        }, label: {
+            Text("\(value)")
+                .frame(width: 40, height: 40)
+                .border(Color.black)
+        })
     }
 }
 
 struct GridRow: View {
+    
+    @Binding var data: [Int]
+    
     var body: some View {
         HStack(spacing: 0) {
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
-            GridBox()
+            GridBox(value: data[0])
+            GridBox(value: data[1])
+            GridBox(value: data[2])
+            GridBox(value: data[3])
+            GridBox(value: data[4])
+            GridBox(value: data[5])
+            GridBox(value: data[6])
+            GridBox(value: data[7])
+            GridBox(value: data[8])
+//            GridBox()
+//            GridBox()
+//            GridBox()
+//            GridBox()
+//            GridBox()
+//            GridBox()
+//            GridBox()
+//            GridBox()
         }
     }
 }
 
 #Preview {
-    GridView()
+    @State var row1: [Int] = Array(repeating: 1, count: 9)
+    @State var row2: [Int] = Array(repeating: 2, count: 9)
+    @State var row3: [Int] = Array(repeating: 3, count: 9)
+    
+    @State var row4: [Int] = Array(repeating: 4, count: 9)
+    @State var row5: [Int] = Array(repeating: 5, count: 9)
+    @State var row6: [Int] = Array(repeating: 6, count: 9)
+    
+    @State var row7: [Int] = Array(repeating: 7, count: 9)
+    @State var row8: [Int] = Array(repeating: 8, count: 9)
+    @State var row9: [Int] = Array(repeating: 9, count: 9)
+    
+    @State var gridData: [Binding<[Int]>] = [$row1, 
+                                             $row2,
+                                             $row3,
+                                             $row4,
+                                             $row5,
+                                             $row6,
+                                             $row7,
+                                             $row8,
+                                             $row9
+    ]
+    
+    return GridView(data: $gridData)
 }
